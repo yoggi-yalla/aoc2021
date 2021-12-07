55847e02cf9e6e0f18fbaf3e8ed1bd393f370049
Oskar Petersen <oskar@Oskars-MacBook-Air.local>
2021-12-07 12:13:52 +0100

    day7 refactored

--- d7/solution.py
+++ d7/solution.py
@@ -1,27 +1,17 @@
-from functools import cache
-
 with open('input.txt') as f:
     data = f.read()
 
-@cache
-def cached_sum_range(diff):
-    return sum(range(1, diff + 1))
-
-
 nums = [int(x) for x in data.split(',')]
-left = min(nums)
-right = max(nums)
-
 
 costs_1 = []
 costs_2 = []
-for i in range(left, right):
+for i in range(min(nums), max(nums)):
     cost_1 = 0
     cost_2 = 0
     for num in nums:
         diff = abs(i - num)
         cost_1 += diff
-        cost_2 += cached_sum_range(diff)
+        cost_2 += diff * (diff + 1) // 2
     costs_1.append(cost_1)
     costs_2.append(cost_2)
 print("Part 1:", min(costs_1))
