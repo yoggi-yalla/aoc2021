70353262ceb9d8c56210ca1c3e591aaf738dcd73
Oskar Petersen <oskar@Oskars-MacBook-Air.local>
2021-12-11 17:15:35 +0100

    day11

--- 11/solution.py
+++ 11/solution.py
@@ -23,7 +23,7 @@ def add_1(grid):
         for y in range(size):
             grid[x][y] += 1
 
-def fill_buffer(grid):
+def get_buffer(grid):
     buffer = []
     for x in range(size):
         for y in range(size):
@@ -39,7 +39,7 @@ def emit_light(grid, buffer):
         grid[x][y] = 0
         for dx, dy in directions:
             x2, y2 = x + dx, y + dy
-            if is_out_of_bounds(x2,y2):
+            if is_out_of_bounds(x2, y2):
                 continue
             if grid[x2][y2] != 0:
                 grid[x2][y2] += 1
@@ -51,13 +51,13 @@ iteration = 0
 total_count = 0
 while 1:
     add_1(grid)
-    buffer = fill_buffer(grid)
+    buffer = get_buffer(grid)
     current_count = emit_light(grid, buffer)
     total_count += current_count
 
     iteration += 1
     if iteration == 100:
-        print("Part 1", total_count)
+        print("Part 1:", total_count) # 1640
     if current_count == 100:
-        print("Part 2:", iteration)
+        print("Part 2:", iteration) # 312
         break
