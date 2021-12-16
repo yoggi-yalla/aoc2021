29d907b32ddaaacd490112b754a69aa285671f75
Oskar Petersen <oskar@Oskars-MacBook-Air.local>
2021-12-16 20:53:20 +0100

    day15 faster

--- 15/solution.py
+++ 15/solution.py
@@ -1,9 +1,7 @@
-from collections import defaultdict
+import heapq
 
 with open('input.txt') as f:
     data = f.read()
-
-grid_1 = [[int(c) for c in line] for line in data.splitlines()]
 
 directions = [
     ( 1, 0),
@@ -12,53 +10,50 @@ directions = [
     ( 0,-1)
 ]
 
-def is_out_of_bounds(x, y, size):
-    return x >= size or y >= size or x < 0 or y < 0
+def neighbors(x, y):
+    for dx, dy in directions:
+        yield (x + dx, y + dy)
 
 def run(grid):
     size = len(grid)
 
-    visited = set()
-    cost_map = defaultdict(lambda: float('inf'))
-    cost_map[(0, 0)] = 0
+    horizon = [(0, (0, 0))]
+    horizon_set = set(horizon)
+    remaining_nodes = set([(i, j) for i in range(size) for j in range(size)])
 
-    while cost_map:
-        x, y = min(cost_map, key=cost_map.get)
-        visited.add((x, y))
-        cost = cost_map.pop((x, y))
-        for dx, dy in directions:
-            x2, y2 = x + dx, y + dy
-            if (x2, y2) in visited or is_out_of_bounds(x2, y2, size):
+    cost_grid = [[float('inf') for _ in range(size)] for _ in range(size)]
+    cost_grid[0][0] = 0
+
+    while horizon:
+        cost, (x, y) = heapq.heappop(horizon)
+        remaining_nodes.remove((x, y))
+        horizon_set.remove((cost, (x, y)))
+        for x2, y2 in neighbors(x, y):
+            if (x2, y2) not in remaining_nodes:
                 continue
             added_cost = grid[x2][y2]
-            cost_map[(x2, y2)] = min(cost_map[(x2, y2)], cost + added_cost)
+            cost_grid[x2][y2] = min(cost_grid[x2][y2], cost + added_cost)
+            if (cost_grid[x2][y2], (x2, y2)) not in horizon_set:
+                horizon_set.add((cost_grid[x2][y2], (x2, y2)))
+                heapq.heappush(horizon, (cost_grid[x2][y2], (x2, y2)))
 
-    return cost
+    return cost_grid[-1][-1]
 
-print("Part 1:", run(grid_1))
+
+grid_1 = [[int(c) for c in line] for line in data.splitlines()]
+print("Part 1:", run(grid_1)) # 592
 
 
 grid_2 = []
-for line in grid_1:
-    new_line = []
-    for i in range(5):
-        for num in line:
-            new_num = (num + i)
-            if new_num > 9:
-                new_num = new_num % 9
-            new_line.append(new_num)
-    grid_2.append(new_line)
-
-additions = []
-for i in range(1, 5):
-    for line in grid_2:
+for i in range(5):
+    for line in grid_1:
         new_line = []
-        for num in line:
-            new_num = (num + i)
-            if new_num > 9:
-                new_num = new_num % 9
-            new_line.append(new_num)
-        additions.append(new_line)
-grid_2 += additions
+        for j in range(5):
+            for num in line:
+                new_num = num + i + j
+                if new_num > 9:
+                    new_num %= 9
+                new_line.append(new_num)
+        grid_2.append(new_line)
 
-print("Part 2:", run(grid_2,)) # 2897
+print("Part 2:", run(grid_2)) # 2897
