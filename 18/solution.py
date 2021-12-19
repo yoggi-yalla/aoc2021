08cd50a4b671c635585e7b706b14aecc78e56097
Oskar Petersen <oskar@Oskars-MacBook-Air.local>
2021-12-19 14:32:32 +0100

    day18

--- 18/solution.py
+++ 18/solution.py
@@ -1,11 +1,8 @@
-import copy
-
 with open('input.txt') as f:
     data = f.read()
 
 class Node:
     def __init__(self):
-        self.up = None
         self.left = None
         self.right = None
         self.value = None
@@ -27,45 +24,48 @@ def parse_num(line, depth=0):
 
     return num
 
-def make_tree(num, parent):
+def make_tree(num):
     this = Node()
-    this.up = parent
     if type(num) == int:
         this.value = num
         return this
-    
-    this.left = make_tree(num[0], this)
-    this.right = make_tree(num[1], this)
 
+    this.left = make_tree(num[0])
+    this.right = make_tree(num[1])
     return this
-
-nums = [parse_num(line) for line in data.splitlines()]
-trees_1 = []
-for num in nums:
-    trees_1.append(make_tree(num, None))
-trees_2 = copy.deepcopy(trees_1)
-
 
 def add(tree1, tree2):
     new_root = Node()
-    tree1.up = new_root
-    tree2.up = new_root
     new_root.left = tree1
     new_root.right = tree2
     return new_root
 
-def find_exploding_node(node, depth):
-    if depth == 5:
-        return node.up
-    if node.value is not None:
-        return None
-    return (
-        find_exploding_node(node.left, depth + 1)
-        or find_exploding_node(node.right, depth + 1)
-    )
+def find_exploding_node(node):
+    left = None
+    target = None
+    right = None
+    horizon = [(node, 0)]
+    while horizon:
+        node, depth = horizon.pop()
+
+        if depth == 4 and not target and node.value is None:
+            target = node
+            continue
+
+        if not target and node.value is not None:
+            left = node
+        
+        if target and node.value is not None:
+            right = node
+            return left, target, right
+        
+        if node.left and node.right:
+            horizon += [(node.right, depth + 1), (node.left, depth + 1)]
+    
+    return left, target, right
 
 def find_splitting_node(node):
-    if not node.value is None:
+    if node.value is not None:
         if node.value > 9:
             return node
         else:
@@ -75,48 +75,17 @@ def find_splitting_node(node):
         or find_splitting_node(node.right)
     )
 
-def find_left_ancestor(node, child):
-    if node is None:
-        return None
-    if node.left is not child:
-        return node.left
-    return find_left_ancestor(node.up, node)
-
-def find_right_ancestor(node, child):
-    if node is None:
-        return None
-    if node.right is not child:
-        return node.right
-    return find_right_ancestor(node.up, node)
-
-def bottom_right_node(node):
-    while node.right:
-        node = node.right
-    return node
-    
-def bottom_left_node(node):
-    while node.left:
-        node = node.left
-    return node
-
 def reduce(tree):
     while 1:
-        exploding_node = find_exploding_node(tree, 0)
+        left, exploding_node, right = find_exploding_node(tree)
         if exploding_node:
-            left_ancestor = find_left_ancestor(exploding_node.up, exploding_node)
-            if left_ancestor:
-                left_node = bottom_right_node(left_ancestor)
-                left_node.value += exploding_node.left.value
-
-            right_ancestor = find_right_ancestor(exploding_node.up, exploding_node)
-            if right_ancestor:
-                right_node = bottom_left_node(right_ancestor)
-                right_node.value += exploding_node.right.value
-
+            if left:
+                left.value += exploding_node.left.value
+            if right:
+                right.value += exploding_node.right.value
             exploding_node.left = None
             exploding_node.right = None
             exploding_node.value = 0
-
             continue
 
         splitting_node = find_splitting_node(tree)
@@ -124,18 +93,13 @@ def reduce(tree):
             left = Node()
             right = Node()
 
-            left.up = splitting_node
-            right.up = splitting_node
-
             left.value = int(splitting_node.value / 2)
             right.value = int(0.5 + splitting_node.value / 2)
 
             splitting_node.value = None
             splitting_node.left = left
             splitting_node.right = right
-
             continue
-        
         break
           
     return tree
@@ -146,21 +110,24 @@ def get_magnitude(node):
     return 3 * get_magnitude(node.left) + 2 * get_magnitude(node.right)
 
 
-tree_1 = trees_1[0]
-for t in trees_1[1:]:
-    tree_1 = add(tree_1, t)
-    tree_1 = reduce(tree_1)
-print("Part 1:", get_magnitude(tree_1))
+nums = [parse_num(line) for line in data.splitlines()] # snailfish numbers as lists
+trees = [make_tree(num) for num in nums] # snailfish numbers as trees
+
+
+full_tree = trees[0]
+for tree in trees[1:]:
+    full_tree = add(full_tree, tree)
+    full_tree = reduce(full_tree)
+print("Part 1:", get_magnitude(full_tree)) # 3305
 
 
 largest_magnitude = 0
-for i in range(len(trees_2)):
-    for j in range(len(trees_2)):
-        if i == j:
+for num1 in nums:
+    for num2 in nums:
+        if num1 is num2:
             continue
-        t1 = copy.deepcopy(trees_2[i])
-        t2 = copy.deepcopy(trees_2[j])
-
-        mag = get_magnitude(reduce(add(t1, t2)))
-        largest_magnitude = max(largest_magnitude, mag)
-print("Part 2:", largest_magnitude)
+        t1 = make_tree(num1)
+        t2 = make_tree(num2)
+        magnitude = get_magnitude(reduce(add(t1, t2)))
+        largest_magnitude = max(largest_magnitude, magnitude)
+print("Part 2:", largest_magnitude) # 4563
