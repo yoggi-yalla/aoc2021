e3646ccb9769c38cb430cd0888214a6238f465da
Oskar Petersen <oskar@Oskars-MacBook-Air.local>
2021-12-25 23:09:26 +0100

    day23

--- 23/solution.py
+++ 23/solution.py
@@ -69,33 +69,36 @@ def get_next_states(state, cost, room_size):
         if letter == '.':
             continue
         
+        letter_cost = letter_to_cost[letter]
+
         room_index = letter_to_index[letter]
         door_index = doors[room_index]
-        letter_cost = letter_to_cost[letter]
         room = state[room_index]
 
         if len(room) == 0 or all(c == letter for c in room):
             if i > door_index:    
                 if all(c == '.' for c in state.hallway[door_index:i]):
-                    new_cost = (i - door_index) * letter_cost + (room_size - len(room)) * letter_cost
+                    added_cost = (i - door_index) * letter_cost + (room_size - len(room)) * letter_cost
                 else:
                     continue
 
             if i < door_index:
                 if all(c == '.' for c in state.hallway[i + 1:door_index]):
-                    new_cost = (door_index - i) * letter_cost + (room_size - len(room)) * letter_cost
+                    added_cost = (door_index - i) * letter_cost + (room_size - len(room)) * letter_cost
                 else:
                     continue
-        
-            new_hallway = tuple([*state.hallway[:i], '.', *state.hallway[i + 1:]])
-            new_room = tuple([*room, letter])
+
+            new_cost = cost + added_cost
+            new_hallway = state.hallway[:i] + ('.',) + state.hallway[i + 1:]
+            new_room = room + (letter,)
+
             new_state = State(
                 *state[:room_index],
                 new_room,
-                *state[room_index+1:-1],
+                *state[room_index + 1:-1],
                 new_hallway
             )
-            next_states.append((cost + new_cost, new_state))
+            next_states.append((new_cost, new_state))
     
     for i in range(4):
         room = state[i]
@@ -110,28 +113,25 @@ def get_next_states(state, cost, room_size):
         for spot in hallway_spots:
             if spot < door_index:
                 if all(c == '.' for c in state.hallway[spot:door_index]):
-                    new_cost = (room_size + 1 - len(room)) * letter_cost + (door_index - spot) * letter_cost
+                    added_cost = (room_size + 1 - len(room)) * letter_cost + (door_index - spot) * letter_cost
                 else:
                     continue
 
             if spot > door_index:
                 if all(c == '.' for c in state.hallway[door_index:spot+1]):
-                    new_cost = (room_size + 1 - len(room)) * letter_cost + (spot - door_index) * letter_cost
+                    added_cost = (room_size + 1 - len(room)) * letter_cost + (spot - door_index) * letter_cost
                 else:
                     continue
 
-            new_hallway = tuple([
-                *state.hallway[:spot],
-                letter,
-                *state.hallway[spot+1:]
-            ])
+            new_cost = cost + added_cost
+            new_hallway = state.hallway[:spot] + (letter,) + state.hallway[spot+1:]
             new_state = State(
                 *state[:i],
                 new_room,
                 *state[i+1:-1],
                 new_hallway
             )
-            next_states.append((cost + new_cost, new_state))
+            next_states.append((new_cost, new_state))
 
     return next_states
 
@@ -139,6 +139,7 @@ def get_next_states(state, cost, room_size):
 def run(start_state, terminal_state, room_size):
     visited = set()
     horizon = [(0, start_state)]
+
     while horizon:
         cost, state = heapq.heappop(horizon)
         if state == terminal_state:
