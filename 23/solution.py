9e7cdce484dc32da307aff7ea66ef9b372a16d1f
Oskar Petersen <oskar@Oskars-MacBook-Air.local>
2021-12-29 15:12:14 +0100

    day23

--- 23/solution.py
+++ 23/solution.py
@@ -1,131 +1,110 @@
-from collections import namedtuple
 import heapq
 
-State = namedtuple(
-    'State', [
-        'room1',
-        'room2',
-        'room3',
-        'room4',
-        'hallway',
-    ]
-)
-
-letter_to_cost = {
-    'A': 1,
-    'B': 10,
-    'C': 100,
-    'D': 1000 
-}
-
-letter_to_index = {
-    'A': 0,
-    'B': 1,
-    'C': 2,
-    'D': 3
-}
-
-hallway_spots = (0, 1, 3, 5, 7, 9, 10)
+letters = ('A', 'B', 'C', 'D')
+costs = (1, 10, 100, 1000)
 doors = (2, 4, 6, 8)
 
+hallway_spots = (0, 1, 3, 5, 7, 9, 10)
 
-start_state_1 = State(
-    ('B', 'D'),
-    ('C', 'C'),
-    ('D', 'A'),
-    ('A', 'B'),
-    ('.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'),
+start_state_1 = (
+    "BD",
+    "CC",
+    "DA",
+    "AB",
+    "..........."
 )
 
-terminal_state_1 = State(
-    ('A', 'A'),
-    ('B', 'B'),
-    ('C', 'C'),
-    ('D', 'D'),
-    ('.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'),
+terminal_state_1 = (
+    "AA",
+    "BB",
+    "CC",
+    "DD",
+    "..........."
 )
 
-start_state_2 = State(
-    ('B', 'D', 'D', 'D'),
-    ('C', 'B', 'C', 'C'),
-    ('D', 'A', 'B', 'A'),
-    ('A', 'C', 'A', 'B'),
-    ('.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'),
+start_state_2 = (
+    "BDDD",
+    "CBCC",
+    "DABA",
+    "ACAB",
+    "..........."
 )
 
-terminal_state_2 = State(
-    ('A', 'A', 'A', 'A'),
-    ('B', 'B', 'B', 'B'),
-    ('C', 'C', 'C', 'C'),
-    ('D', 'D', 'D', 'D'),
-    ('.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'),
+terminal_state_2 = (
+    "AAAA",
+    "BBBB",
+    "CCCC",
+    "DDDD",
+    "..........."
 )
 
 
 def get_next_states(state, cost, room_size):
     next_states = []
-    for i in range(11):
-        letter = state.hallway[i]
+    hallway = state[4]
+
+    for i in hallway_spots:
+        letter = hallway[i]
         if letter == '.':
             continue
         
-        letter_cost = letter_to_cost[letter]
+        dest_index = letters.index(letter)
 
-        room_index = letter_to_index[letter]
-        door_index = doors[room_index]
-        room = state[room_index]
+        letter_cost = costs[dest_index]
+        door_index = doors[dest_index]
+        dest_room = state[dest_index]
 
-        if len(room) == 0 or all(c == letter for c in room):
-            if i > door_index:    
-                if all(c == '.' for c in state.hallway[door_index:i]):
-                    added_cost = (i - door_index) * letter_cost + (room_size - len(room)) * letter_cost
+        if len(dest_room) == 0 or dest_room == len(dest_room) * letters[dest_index]:
+            if i > door_index:
+                if hallway[door_index:i] == "." * len(hallway[door_index:i]):
+                    added_cost = (i - door_index) * letter_cost + (room_size - len(dest_room)) * letter_cost
                 else:
                     continue
 
             if i < door_index:
-                if all(c == '.' for c in state.hallway[i + 1:door_index]):
-                    added_cost = (door_index - i) * letter_cost + (room_size - len(room)) * letter_cost
+                if hallway[i + 1:door_index] == "." * len(hallway[i + 1:door_index]):
+                    added_cost = (door_index - i) * letter_cost + (room_size - len(dest_room)) * letter_cost
                 else:
                     continue
 
             new_cost = cost + added_cost
-            new_hallway = state.hallway[:i] + ('.',) + state.hallway[i + 1:]
-            new_room = room + (letter,)
+            new_hallway = hallway[:i] + '.' + hallway[i + 1:]
+            new_room = dest_room + letter
 
-            new_state = State(
-                *state[:room_index],
+            new_state = (
+                *state[:dest_index],
                 new_room,
-                *state[room_index + 1:-1],
+                *state[dest_index + 1:-1],
                 new_hallway
             )
             next_states.append((new_cost, new_state))
     
     for i in range(4):
         room = state[i]
-        if len(room) == 0:
+        if len(room) == 0 or room == len(room) * letters[i]:
             continue
         
         letter = room[-1]
-        letter_cost = letter_to_cost[letter]
+        letter_cost = costs[letters.index(letter)]
         new_room = room[:-1]
         door_index = doors[i]
 
         for spot in hallway_spots:
             if spot < door_index:
-                if all(c == '.' for c in state.hallway[spot:door_index]):
+                if hallway[spot:door_index] == len(hallway[spot:door_index]) * '.':
                     added_cost = (room_size + 1 - len(room)) * letter_cost + (door_index - spot) * letter_cost
                 else:
                     continue
 
             if spot > door_index:
-                if all(c == '.' for c in state.hallway[door_index:spot+1]):
+                if hallway[door_index:spot+1] == len(hallway[door_index:spot+1]) * '.':
                     added_cost = (room_size + 1 - len(room)) * letter_cost + (spot - door_index) * letter_cost
                 else:
                     continue
 
             new_cost = cost + added_cost
-            new_hallway = state.hallway[:spot] + (letter,) + state.hallway[spot+1:]
-            new_state = State(
+            new_hallway = hallway[:spot] + letter + hallway[spot+1:]
+            new_state = (
                 *state[:i],
                 new_room,
                 *state[i+1:-1],
@@ -136,12 +115,34 @@ def get_next_states(state, cost, room_size):
     return next_states
 
 
+def get_expected_cost(state):
+    cost = 0
+    for i in range(4):
+        pos = doors[i]
+        room = state[i]
+        for c in room:
+            dest_index = letters.index(c)
+            dest = doors[dest_index]
+            if pos == dest:
+                continue
+            cost += costs[dest_index] * (abs(pos - dest) + 2)
+    hallway = state[4]
+    for pos in hallway_spots:
+        letter = hallway[pos]
+        if letter == '.':
+            continue
+        dest_index = letters.index(letter)
+        dest = doors[dest_index]
+        cost += costs[dest_index] * (abs(pos - dest) + 1)
+    return cost
+
+
 def run(start_state, terminal_state, room_size):
     visited = set()
-    horizon = [(0, start_state)]
+    horizon = [(get_expected_cost(start_state), 0, start_state)]
 
     while horizon:
-        cost, state = heapq.heappop(horizon)
+        _, cost, state = heapq.heappop(horizon)
         if state == terminal_state:
             break
 
@@ -150,9 +151,10 @@ def run(start_state, terminal_state, room_size):
 
         visited.add(state)
 
-        next_states = get_next_states(state, cost, room_size)
-        for state in next_states:
-            heapq.heappush(horizon, state)
+        for next_cost, next_state in get_next_states(state, cost, room_size):
+            if next_state not in visited:
+                next_expected_cost = next_cost + get_expected_cost(next_state)
+                heapq.heappush(horizon, (next_expected_cost, next_cost, next_state))
     
     return cost
 
